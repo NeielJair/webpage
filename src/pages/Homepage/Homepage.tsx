135e2c929c4713a6003ece8d5d336ca9fa849d1d
NeielJair <neieljair.ua@gmail.com>
2023-11-19 10:25:26 -0300

    feat(webpage): centered

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,3 +1,9 @@
+import { Grid } from '@mui/material';
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <Grid container height="100%" direction="row" justifyContent="center" alignItems="center">
+      <Grid item>Homepage</Grid>
+    </Grid>
+  );
 }
