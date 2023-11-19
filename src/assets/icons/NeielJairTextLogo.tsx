b2a65d48044e406a9365b0669e43c567d133f62e
NeielJair <neieljair.ua@gmail.com>
2023-11-19 12:09:26 -0300

    fix: typo in text

--- src/assets/icons/NeielJairTextLogo.tsx
+++ src/assets/icons/NeielJairTextLogo.tsx
@@ -2,5 +2,5 @@ import { SVGProps } from 'react';
 import { ReactComponent as TextLogoSVG } from '../images/neieljair-text-logo.svg';
 
 export default function NeielJairTextLogo(props: SVGProps<SVGSVGElement>) {
-  return <TextLogoSVG style={{ filter: 'invert(1)' }} {...props} />;
+  return <TextLogoSVG style={{ filter: 'invert(1)', fontFamily: 'Source Code Pro' }} {...props} />;
 }
